895faeb380d37501f031a5e381e41e587a7b6008
Gaoyang Li <gaoyangli@hit.edu.cn>
2026-04-02 16:33:28 +0800

    Update HitSV script for multi-processing and logging

--- HitSV_multy_process.sh
+++ HitSV_multy_process.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+#SBATCH --mem=50g
+#SBATCH --cpus-per-task=24
 
 INPUT=$1
 WORK_DIR=$2
 PRESET=$3
 REF=$4
 
-GCSV_TOOL="./HitSV call "
+HITSV_TOOL="HitSV call "
 
 mkdir ${WORK_DIR}
 cd ${WORK_DIR}
@@ -18,16 +20,23 @@ LOG=${WORK_DIR}/D.log
 #call_single_chr
 task_call_single_chr() {
     echo "Starting task task_call_single_chr-$1"
-    ${GCSV_TOOL} -S $1 -E $1 -p ${PRESET} -l ${BAM_ORI} -r ${REF} -o ${WORK_DIR}/PART_$1_D.vcf 2> /dev/null
-    echo "Finished task task_call_single_chr-$1"
-}
+        ${HITSV_TOOL} -S $1 -E $1 -p ${PRESET} -l ${BAM_ORI} -r ${REF} -o ${WORK_DIR}/PART_$1_D.vcf >${WORK_DIR}/PART_$1_D_1.log 2> ${WORK_DIR}/PART_$1_D_2.log
+            echo "Finished task task_call_single_chr-$1"
+            }
 
-#combine
-task_combine() {
-    echo "Starting task combine"
-    cat ${WORK_DIR}/PART_0_D.vcf > ${VCF}
-    for i in {1..23}; do
-        echo ${WORK_DIR}/PART_${i}_D.vcf
-        cat ${WORK_DIR}/PART_${i}_D.vcf | grep -v "#" >> ${VCF}
-    done
-    echo "Finished task combine"
+            #combine
+            task_combine() {
+                echo "Starting task combine"
+                    cat ${WORK_DIR}/PART_0_D.vcf > ${VCF}
+                        for i in {1..23}; do
+                                echo ${WORK_DIR}/PART_${i}_D.vcf
+                                        cat ${WORK_DIR}/PART_${i}_D.vcf | grep -v "#" >> ${VCF}
+                                            done
+                                                echo "Finished task combine"
+                                                }
+
+                                                for i in {0..23}; do
+                                                    task_call_single_chr $i &
+                                                    done
+                                                    wait
+                                                    task_combine

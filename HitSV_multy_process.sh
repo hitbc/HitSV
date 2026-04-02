fb87b13f6f36f32e98f7cf49f2bd52afc4c5a668
Gaoyang Li <gaoyangli@hit.edu.cn>
2026-04-02 16:37:28 +0800

    Update HitSV_multy_process.sh

--- HitSV_multy_process.sh
+++ HitSV_multy_process.sh
@@ -7,36 +7,36 @@ WORK_DIR=$2
 PRESET=$3
 REF=$4
 
-HITSV_TOOL="HitSV call "
+HitSV_TOOL="HitSV call "
 
 mkdir ${WORK_DIR}
 cd ${WORK_DIR}
 
 BAM_ORI=${INPUT}
 
-VCF=${WORK_DIR}/D.vcf
-LOG=${WORK_DIR}/D.log
+VCF=${WORK_DIR}/HitSV.vcf
+LOG=${WORK_DIR}/HitSV.log
 
 #call_single_chr
 task_call_single_chr() {
     echo "Starting task task_call_single_chr-$1"
-        ${HITSV_TOOL} -S $1 -E $1 -p ${PRESET} -l ${BAM_ORI} -r ${REF} -o ${WORK_DIR}/PART_$1_D.vcf >${WORK_DIR}/PART_$1_D_1.log 2> ${WORK_DIR}/PART_$1_D_2.log
-            echo "Finished task task_call_single_chr-$1"
-            }
+    ${HitSV_TOOL} -S $1 -E $1 -p ${PRESET} -l ${BAM_ORI} -r ${REF} -o ${WORK_DIR}/PART_$1_D.vcf >${WORK_DIR}/PART_$1_D_1.log 2> ${WORK_DIR}/PART_$1_D_2.log
+    echo "Finished task task_call_single_chr-$1"
+}
 
-            #combine
-            task_combine() {
-                echo "Starting task combine"
-                    cat ${WORK_DIR}/PART_0_D.vcf > ${VCF}
-                        for i in {1..23}; do
-                                echo ${WORK_DIR}/PART_${i}_D.vcf
-                                        cat ${WORK_DIR}/PART_${i}_D.vcf | grep -v "#" >> ${VCF}
-                                            done
-                                                echo "Finished task combine"
-                                                }
+#combine
+task_combine() {
+    echo "Starting task combine"
+    cat ${WORK_DIR}/PART_0_D.vcf > ${VCF}
+    for i in {1..23}; do
+        echo ${WORK_DIR}/PART_${i}_D.vcf
+        cat ${WORK_DIR}/PART_${i}_D.vcf | grep -v "#" >> ${VCF}
+    done
+    echo "Finished task combine"
+}
 
-                                                for i in {0..23}; do
-                                                    task_call_single_chr $i &
-                                                    done
-                                                    wait
-                                                    task_combine
+for i in {0..23}; do
+    task_call_single_chr $i &
+done
+wait
+task_combine
